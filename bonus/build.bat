c05746dbe284bf35734259cc104bb3fb32c07a23
Spencer Isaacson <spencer_isaacson@yahoo.com>
2025-05-16 15:48:08 -0600

    just a build system comment, the build system is currently a bit ad hoc, this should all be unified under a common build system if possible

--- bonus/build.bat
+++ bonus/build.bat
@@ -1,2 +1,4 @@
+REM this should be added to the standard makefile, and in fact the whole build system should be unified
+
 @REM gcc -O2 gui.c -o gui.exe -luser32 -lgdi32 -Wl,-subsystem,console
 tcc gui.c -Wl,-subsystem=console
